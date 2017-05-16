9f74bfc5366c79700bea0e5b444d8d6e109fc187
Balfear <lukaspessl@gmx.net>
2017-05-16 16:23:09 +0200

    test

--- images/plone/docker-entrypoint.sh
+++ images/plone/docker-entrypoint.sh
@@ -10,20 +10,11 @@ python /docker-initialize.py
 if [ -e "custom.cfg" ]; then
   if [ ! -e "bin/develop" ]; then
     bin/buildout -c custom.cfg
-    python /docker-initialize.py
   fi
 fi
 
 if [[ "$1" == "zeo"* ]]; then
   CMD="bin/zeoserver"
-fi
-
-if [ -z "$HEALTH_CHECK_TIMEOUT" ]; then
-  HEALTH_CHECK_TIMEOUT=1
-fi
-
-if [ -z "$HEALTH_CHECK_INTERVAL" ]; then
-  HEALTH_CHECK_INTERVAL=1
 fi
 
 if [[ $START == *"$1"* ]]; then
@@ -35,19 +26,9 @@ if [[ $START == *"$1"* ]]; then
   trap _stop SIGTERM SIGINT
   $CMD start
   $CMD logtail &
-  child=$!
 
-  pid=`$CMD status | sed 's/[^0-9]*//g'`
-  if [ ! -z "$pid" ]; then
-    echo "Application running on pid=$pid"
-    sleep "$HEALTH_CHECK_TIMEOUT"
-    while kill -0 "$pid" 2> /dev/null; do
-      sleep "$HEALTH_CHECK_INTERVAL"
-    done
-  else
-    echo "Application didn't start normally. Shutting down!"
-    _stop
-  fi
+  child=$!
+  wait "$child"
 else
   if [[ $COMMANDS == *"$1"* ]]; then
     exec bin/instance "$@"
--- images/plone/docker-initialize.py
+++ images/plone/docker-initialize.py
@@ -2,23 +2,16 @@
 
 import re
 import os
-import warnings
-warnings.simplefilter('always', DeprecationWarning)
 
 class Environment(object):
     """ Configure container via environment variables
     """
     def __init__(self, env=os.environ,
                  zope_conf="/plone/instance/parts/instance/etc/zope.conf",
-                 custom_conf="/plone/instance/custom.cfg",
-                 zeopack_conf="/plone/instance/bin/zeopack",
-                 zeoserver_conf="/plone/instance/parts/zeoserver/etc/zeo.conf"
-                 ):
+                 custom_conf="/plone/instance/custom.cfg"):
         self.env = env
         self.zope_conf = zope_conf
         self.custom_conf = custom_conf
-        self.zeopack_conf = zeopack_conf
-        self.zeoserver_conf = zeoserver_conf
 
     def zeoclient(self):
         """ ZEO Client
@@ -55,43 +48,6 @@ class Environment(object):
         with open(self.zope_conf, "w") as cfile:
             cfile.write(config)
 
-    def zeopack(self):
-        """ ZEO Pack
-        """
-        server = self.env.get("ZEO_ADDRESS", None)
-        if not server:
-            return
-
-        if ":" in server:
-            host, port = server.split(":")
-        else:
-            host, port = (server, "8100")
-
-        with open(self.zeopack_conf, 'r') as cfile:
-            text = cfile.read()
-            text = text.replace('address = "8100"', 'address = "%s"' % server)
-            text = text.replace('host = "127.0.0.1"', 'host = "%s"' % host)
-            text = text.replace('port = "8100"', 'port = "%s"' % port)
-
-        with open(self.zeopack_conf, 'w') as cfile:
-            cfile.write(text)
-
-    def zeoserver(self):
-        """ ZEO Server
-        """
-        pack_keep_old = self.env.get("ZEO_PACK_KEEP_OLD", '')
-        if pack_keep_old.lower() in ("false", "no", "0", "n", "f"):
-            with open(self.zeoserver_conf, 'r') as cfile:
-                text = cfile.read()
-                if 'pack-keep-old' not in text:
-                    text = text.replace(
-                        '</filestorage>',
-                        '  pack-keep-old false\n</filestorage>'
-                    )
-
-            with open(self.zeoserver_conf, 'w') as cfile:
-                cfile.write(text)
-
     def buildout(self):
         """ Buildout from environment variables
         """
@@ -99,32 +55,9 @@ class Environment(object):
         if os.path.exists(self.custom_conf):
             return
 
-        eggs = self.env.get("PLONE_ADDONS",
-               self.env.get("ADDONS", "")).strip().split()
-        if not eggs:
-            eggs = self.env.get("BUILDOUT_EGGS", "").strip().split()
-            if eggs:
-                warnings.warn(
-                    "BUILDOUT_EGGS is deprecated. Please use "
-                    "PLONE_ADDONS instead !!!", DeprecationWarning)
-
-        zcml = self.env.get("PLONE_ZCML",
-               self.env.get("ZCML", "")).strip().split()
-        if not zcml:
-            zcml = self.env.get("BUILDOUT_ZCML", "").strip().split()
-            if zcml:
-                warnings.warn(
-                    "BUILDOUT_ZCML is deprecated. Please use "
-                    "PLONE_ZCML instead !!!", DeprecationWarning)
-
-        develop = self.env.get("PLONE_DEVELOP",
-                  self.env.get("DEVELOP", "")).strip().split()
-        if not develop:
-            develop = self.env.get("BUILDOUT_DEVELOP", "").strip().split()
-            if develop:
-                warnings.warn(
-                    "BUILDOUT_DEVELOP is deprecated. Please use "
-                    "PLONE_DEVELOP instead !!!", DeprecationWarning)
+        eggs = self.env.get("BUILDOUT_EGGS", "").strip().split()
+        zcml = self.env.get("BUILDOUT_ZCML", "").strip().split()
+        develop = self.env.get("BUILDOUT_DEVELOP", "").strip().split()
 
         if not (eggs or zcml or develop):
             return
@@ -141,8 +74,6 @@ class Environment(object):
     def setup(self, **kwargs):
         self.buildout()
         self.zeoclient()
-        self.zeopack()
-        self.zeoserver()
 
     __call__ = setup
 
